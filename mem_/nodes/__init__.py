ef181c433d55bdf06bcdaea17a4b0be1aff8e777
Scott R. Parish <sparish@peak6.com>
2008-12-22 21:41:21 -0600

    new Env() based on subclassing dict

--- mem_/nodes/__init__.py
+++ mem_/nodes/__init__.py
@@ -38,41 +38,20 @@ class File(object):
         return {"path": self.path,
                 "hash": self.hash}
 
-class Env(object):
-    def __init__(self, **kwargs):
-        self.__dict__["d"] = dict(**kwargs)
-
-    def __getattr__(self, key, *args):
-        if len(args) == 1:
-            return self.__dict__["d"].get(key, args[0])
-        else:
-            return self.__dict__["d"][key]
+class Env(dict):
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError e:
+            raise AttributeError("'Env' objec has no attribute '%s'" % key)
 
     def __setattr__(self, key, val):
-        self.__dict__["d"][key] = val
-
-    def __getitem__(self, key):
-        return self.__dict__["d"][key]
-
-    def __setitem__(self, key):
-        self.__dict__["d"][key] = val
+        self[key] = val
 
     def __repr__(self):
         return "Env(" + " ".join("%s=%s" % (k, repr(v))
-                                 for k,v in self.__dict__["d"].items()) + ")"
+                                 for k,v in self.items()) + ")"
 
     def __str__(self):
         return repr(self)
 
-    def get(self, m, key):
-        m.add_dep(self.dep(key))
-        return self.__dict__["d"][key]
-
-    def has_key(self, key):
-        return self.__dict__["d"].has_key(key)
-
-    def dep(self, key):
-        return self.EnvDep(self, key)
-
-    def deps(self, keys):
-        return [self.EnvDep(self, key) for key in keys]
