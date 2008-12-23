e4cdf0e5e4677b5894b168782e855313d0a18265
Eric Merritt <emerritt@peak6.com>
2008-12-23 17:10:06 -0600

    General improvements to gcc

--- mem_/tasks/gcc.py
+++ mem_/tasks/gcc.py
@@ -7,10 +7,12 @@ import mem
 File = mem.nodes.File
 DepFiles = mem.nodes.DepFiles
 
-def make_depends(source, CFLAGS, CPPPATH):
+def make_depends(target, source, CFLAGS, CPPPATH):
     mem.add_dep(mem.util.convert_to_file(source))
     includes = ["-I" + path for path in CPPPATH]
     args = mem.util.convert_cmd(["gcc"] + CFLAGS +
+                                ["-I" +
+                                 os.path.dirname(target)] +
                                 includes + ["-M", "-o", "-", source])
     print " ".join(args)
     p = subprocess.Popen(args, stdin = PIPE, stdout = PIPE)
@@ -24,30 +26,43 @@ def make_depends(source, CFLAGS, CPPPATH):
 @mem.util.with_env(CFLAGS=[], CPPPATH=[])
 @mem.memoize
 def t_obj(target, source, CFLAGS, CPPPATH):
-    if not os.path.exists(source):
+    if not os.path.exists(str(source)):
         mem.fail("%s does not exist" % source)
 
     mem.add_dep(mem.util.convert_to_file(source))
-    mem.add_dep(make_depends(source, CFLAGS=CFLAGS, CPPPATH=CPPPATH))
+    mem.add_dep(make_depends(target, source, CFLAGS=CFLAGS, CPPPATH=CPPPATH))
     includes = ["-I" + path for path in CPPPATH]
     args = mem.util.convert_cmd(["gcc"] +  CFLAGS + includes +
+                                ["-I" +
+                                 os.path.dirname(target)] +
                                 ["-c", "-o", target, source])
     print " ".join(args)
+
+    mem.util.ensure_file_dir(target)
     if subprocess.call(args) != 0:
         mem.fail()
     return File(target)
 
-@mem.util.with_env(CFLAGS=[])
+@mem.util.with_env(CFLAGS=[], LIBS=[], LIBPATH=[])
 @mem.memoize
-def t_prog(target, objs, CFLAGS=[]):
+def t_prog(target, objs, CFLAGS, LIBS, LIBPATH):
     mem.add_deps(objs)
-    args = ["gcc", "-o", target] + CFLAGS + [o.path for o in objs]
+
+    npaths = map(lambda a: "-L" + str(a), LIBPATH)
+    nlibs = map(lambda a: "-l" + str(a), LIBS)
+
+    args = mem.util.convert_cmd(["gcc", "-o", target] + CFLAGS +
+                                npaths + nlibs + [o.path for o in objs])
+
     print " ".join(args)
+
+    mem.util.ensure_file_dir(target)
     if subprocess.call(args) != 0:
         mem.fail()
     return File(target)
 
-def obj(source_list, env=None, build_dir = None, CFLAGS = None, CPPPATH = None):
+def obj(source_list, env=None, build_dir = None,
+        CFLAGS = None, CPPPATH = None):
     """ Take a list of sources and convert them to a correct object file """
     if not type(source_list) == list:
         source_list = [source_list]
@@ -55,7 +70,7 @@ def obj(source_list, env=None, build_dir = None, CFLAGS = None, CPPPATH = None):
     BuildDir = mem.util.get_build_dir(env, build_dir)
     targets = []
     for source in nslist:
-        (name, ignore) = os.path.splitext(source)
+        (name, ignore) = os.path.splitext(str(source))
         target = os.path.join(BuildDir, name + ".o")
         t_obj(target, source,
               env.get_override("CFLAGS", CFLAGS),
@@ -64,10 +79,13 @@ def obj(source_list, env=None, build_dir = None, CFLAGS = None, CPPPATH = None):
 
     return targets
 
-def prog(target, objs, env=None, build_dir = None):
+def prog(target, objs, env=None, CFLAGS=[], LIBS=[], LIBPATH=[],
+         build_dir = None):
     """ Convert the list of objects into a program given the cflags """
     nobjs = mem.util.flatten(objs)
     BuildDir = mem.util.get_build_dir(env, build_dir)
     ntarget = os.path.join(BuildDir, target)
-    t_prog(ntarget, nobjs, env.CFLAGS)
+    t_prog(ntarget, nobjs, env.get_override("CFLAGS", CFLAGS),
+           env.get_override("LIBS", LIBS),
+           env.get_override("LIBPATH", LIBPATH))
     return File(ntarget)
