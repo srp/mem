4cf818c00c9b704be32eda3ed89d33eb77cc4258
Dave Boutcher <dboutcher@peak6.com>
2009-01-16 09:45:33 -0600

    Allow flags on asciidoc task

--- mem_/tasks/asciidoc.py
+++ mem_/tasks/asciidoc.py
@@ -2,21 +2,27 @@ import os
 import sys
 import commands
 import mem
+import subprocess
+from subprocess import PIPE
 
+@mem.util.with_env(ASCIIDOC_FLAGS=[])
 @mem.memoize
-def t_asciidoc(target, source):
+def t_asciidoc(target, source, ASCIIDOC_FLAGS):
     """ Runs asciidoc compiler on specified files """
     mem.add_dep(mem.util.convert_to_file(source))
-    cmd = "asciidoc --out-file=%s %s" % (str(target), str(source))
-    print cmd
-    (stat, out) = commands.getstatusoutput(cmd)
-    if stat:
-        mem.fail(cmd + ":" + out)
+
+    cmd = mem.util.convert_cmd(["asciidoc","-o",target]+ASCIIDOC_FLAGS+[source])
+
+    print " ".join(cmd)
+
+    mem.util.ensure_file_dir(target)
+    if subprocess.call(cmd) != 0:
+        mem.fail()
 
     return mem.nodes.File(target)
 
 
-def asciidoc(source, build_dir=None, env=None):
+def asciidoc(source, build_dir=None, env=None, ASCIIDOC_FLAGS=[]):
     if not isinstance(source, list):
         source = [source]
 
@@ -28,6 +34,6 @@ def asciidoc(source, build_dir=None, env=None):
         (name, ext) = os.path.splitext(str(src))
         target = os.path.join(BuildDir, name + ".html")
 
-        out.append(t_asciidoc(target, src))
+        out.append(t_asciidoc(target, src, env=env))
 
     return out
