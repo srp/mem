3c98ce2b390027295d74d65292845c2f17bc4786
Scott R. Parish <srp@srparish.net>
2009-02-06 21:56:18 -0600

    drop swig.find_produces, using temp directory instead
    When it comes to languages like java, there's no good way to guess
    what files swig is going to generate; luckily unlikely other build
    systems we don't need to know the resultant files ahead of time, we
    just have to declare them on return, so have swig build into a
    temp directory and then move the files into place and return them.

--- mem_/tasks/swig.py
+++ mem_/tasks/swig.py
@@ -2,6 +2,8 @@ import os.path
 import re
 import subprocess
 from subprocess import PIPE
+import tempfile
+import shutil
 import mem
 
 # Match '%module test', as well as '%module(directors="1") test'
@@ -19,55 +21,34 @@ def make_depends(source, SWIGFLAGS):
     deps = deps[1:] # first element is the target (eg ".c"), drop it
     return [dep for dep in deps if dep != '\\']
 
-def find_produces(target, source, SWIGFLAGS):
-    ret = []
-    src_data = open(str(source)).read()
-    output = re_module.findall(src_data)
-    outdir = os.path.dirname(target)
-    mnames = None
-    if "-python" in SWIGFLAGS and "-noproxy" not in SWIGFLAGS:
-            if mnames is None:
-                mnames = re_module.findall(open(str(source)).read())
-            ret.extend(map(lambda m, d=outdir:
-                                  os.path.join(outdir, m + ".py"), mnames))
-    if "-java" in SWIGFLAGS:
-        if mnames is None:
-            mnames = re_module.findall(open(src).read())
-            java_files = map(lambda m: [m + ".java", m + "JNI.java"], mnames)
-            java_files = mem.util.flatten(java_files)
-        if outdir:
-            java_files = map(lambda j, o=outdir:
-                                  os.path.join(o, j), java_files)
-            java_files = map(env.fs.File, java_files)
-
-
-            ret.extend(java_files)
-    return ret
-
 
 @mem.util.with_env(SWIGFLAGS=[])
 @mem.memoize
-def to_c(target, source, SWIGFLAGS):
+def generate(BuildDir, source, SWIGFLAGS):
     mem.add_dep(mem.util.convert_to_file(source))
     mem.add_deps([mem.nodes.File(f) for f in make_depends(source, SWIGFLAGS)])
 
-    targets = find_produces(target, source, SWIGFLAGS)
-    targets.append(target)
-    args = mem.util.convert_cmd(['swig',
-                                 '-o',
-                                 target,
-                                 '-outdir',
-                                 os.path.dirname(target)] +
-                                SWIGFLAGS + [source])
+    # since we have no way to know all the files that swig will generate,
+    # we have it generate into a temp directory, then we can see what
+    # exact files were produced, and move them to their proper location.
+    mem.util.ensure_file_dir(BuildDir)
+    tmpdir = tempfile.mkdtemp(dir=BuildDir)
 
+    args = mem.util.convert_cmd(['swig', '-outdir', tmpdir] +
+                                SWIGFLAGS + [source])
     print " ".join(args)
 
-    mem.util.ensure_file_dir(target)
     p = subprocess.Popen(args, stdin = PIPE, stdout = PIPE)
     if p.wait() != 0:
         mem.fail()
 
-    return [mem.nodes.File(f) for f in targets]
+    files = os.listdir(tmpdir)
+    for file in files:
+        shutil.move(os.path.join(tmpdir, file), os.path.join(BuildDir, file))
+
+    os.rmdir(tmpdir)
+
+    return [mem.nodes.File(os.path.join(BuildDir, file)) for file in files]
 
 
 def obj(sources, env=None, build_dir=None, **kwargs):
@@ -77,16 +58,14 @@ def obj(sources, env=None, build_dir=None, **kwargs):
     env = env.copy()
     env.update(kwargs)
 
-    nslist = mem.util.flatten(sources)
+    sources = mem.util.flatten(sources)
     BuildDir = mem.util.get_build_dir(env, build_dir)
-    nslist = mem.util.flatten(sources)
 
     targets = []
     threads = []
-    for source in nslist:
+    for source in sources:
         (name, ignore) = os.path.splitext(str(source))
-        target = os.path.join(BuildDir, name + "_wrap.c")
-        t = mem.util.Runable(to_c, target, source, env=env)
+        t = mem.util.Runable(generate, BuildDir, source, env=env)
         t.start()
         threads.append(t)
 
